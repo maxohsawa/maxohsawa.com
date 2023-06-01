2d9f5be3dbb2da664b7e282327b1395351ecf28b
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-01 17:18:16 -0400

    adds favicon

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,14 +1,31 @@
+import { styled } from 'styled-components'
+
+const ProfileFigure = styled.figure`
+  display: flex;
+  justify-content: center;
+`
+
+const ProfileImage = styled.img`
+  border-radius: 100%;
+  width: auto;
+  height: 30vh;
+`
+
+const ProfileCaption = styled.figcaption`
+  display: none;
+`
+
 const About = () => {
   return (
     <>
       <main>
         
-        <figure>
-            <img src='/profile.jpg' />
-            <figcaption>
+        <ProfileFigure>
+            <ProfileImage src='/profile.jpg' />
+            <ProfileCaption>
               brand image of the developer Max Ohsawa, consisting of a profile image and a border
-            </figcaption>
-          </figure>
+            </ProfileCaption>
+          </ProfileFigure>
       </main>
     </>
   )
