ebcc775ce5f555545f4456bebe79c625b210705e
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-10 19:46:59 -0400

    adds logo containers with neu shadow, copyright tag

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,29 +1,80 @@
 import { styled } from 'styled-components'
 
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
 const ContactContainer = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
 `
 
-const LinkedInLogo = styled.img`
-  width: 100px;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  padding: 2px;
+  border-radius: 100%;
 `
-const GithubLogo = styled.img`
-  width: 100px;
+
+const LogoImage = styled.img`
+  width: 100%;
 `
-const TwitterLogo = styled.img`
-  width: 100px;
+
+const CopyrightContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
 `
+
+const links = [
+  {
+    imgSrc: "/linkedin.svg",
+    url: "https://www.linkedin.com/in/maxohsawa"
+  },
+  {
+    imgSrc: "/github.svg",
+    url: "https://www.github.com/maxohsawa"
+  },
+  {
+    imgSrc: "/twitter.svg",
+    url: "https://www.twitter.com/maxohsawa"
+  },
+]
 
 const Footer = () => {
   return (
     <>
-      <ContactContainer>
-        <LinkedInLogo src="/linkedin.svg" />
-        <GithubLogo src="/github.svg" />
-        <TwitterLogo src="/twitter.svg" />
-      </ContactContainer>
+      <Section>
+        <ContactContainer>
+          {links && links.map((link, index) => {
+            return (
+              <Logo 
+                key={`logo${index}`}
+                className="neu-shadow"
+              >
+                <a href={link.url}>
+                  <LogoImage src={link.imgSrc} />
+                </a>
+              </Logo>
+            )
+
+          })}
+          
+        </ContactContainer>
+        <CopyrightContainer>
+          <p>
+            &copy;2023 Max Ohsawa
+          </p>
+        </CopyrightContainer>
+      </Section>
     </>
   )
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,7 +4,7 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 
 const Main = styled.main`
-  height: 70vh;
+  min-height: 70vh;
 `
 
 const Root = () => {
