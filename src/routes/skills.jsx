2a23a023c5bdd0ad8ce24ef00e18146851a07c00
Max Ohsawa <max.ohsawa@gmail.com>
2023-07-03 13:01:07 -0400

    copies content from about to skills to start it off

--- src/routes/skills.jsx
+++ src/routes/skills.jsx
@@ -1,6 +1,90 @@
+import { NavLink } from 'react-router-dom'
+import { styled } from 'styled-components'
+
+const ProfileFigure = styled.figure`
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+  
+`
+
+const ProfileImage = styled.img`
+  border-radius: 100%;
+  width: auto;
+  height: 25vh;
+`
+
+const ProfileCaption = styled.figcaption`
+  display: none;
+`
+
+const CopyContainer = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+`
+const Greeting = styled.p`
+  margin-bottom: 10px;
+  font-size: 1.6rem;
+  font-weight: bold;
+`
+
+const Copy = styled.p`
+  margin: 10px 0;
+  width: auto;
+  font-size: 1.2rem;
+`
+
+const CTA = styled.div`
+  margin: 10px 0;
+  display: flex;
+  justify-content: center;
+  width: 60vw;
+`
+
+const NavLinkItem = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+        <ProfileFigure>
+            <ProfileImage src='/profile.jpg' className="neu-less"/>
+            <ProfileCaption>
+              brand image of the developer Max Ohsawa, consisting of a profile image and a border
+            </ProfileCaption>
+          </ProfileFigure>
+          <CopyContainer>
+            <Greeting>
+              <strong>Hey there!</strong>
+            </Greeting>
+            <Copy>
+              I'm a software engineer with a focus on full stack web development and bringing delightful, accessible, and useful experiences to users.
+            </Copy>
+            <Copy>
+              Come along and check out what web technologies I like working with!
+            </Copy>
+          </CopyContainer>
+          <CTA>
+            <NavLinkItem>
+              <NavLink 
+                to="/skills"
+                className="active"
+                style={
+                  {
+                    width: "60vw",
+                    padding: "15px",
+                    fontSize: "1.2rem"
+                  }
+                }
+                >
+                Projects
+              </NavLink>
+            </NavLinkItem>
+          </CTA>
+    </>
   )
 }
 
